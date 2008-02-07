3b0238f25e1c9a84027b833b716d003e2030fdae
jserv <jserv@ac1be623-90ea-0310-9410-ba68b2efa777>
2008-02-07 14:55:49 +0000

    2008-02-07  Jim Huang  <jserv.tw@gmail.com>
            * include/chewingio.h,
            include/global.h,
            src/chewingio.c,
            src/hash.c,
            src/choice.c,
            src/porting_layer/include/plat_mmap.h,
            src/porting_layer/src/plat_mmap_win32.c,
            src/porting_layer/src/plat_mmap_posix.c: Update copyright date.
    
    
    git-svn-id: https://svn.csie.net/chewing/libchewing/trunk@820 ac1be623-90ea-0310-9410-ba68b2efa777

--- src/porting_layer/src/plat_mmap_win32.c
+++ src/porting_layer/src/plat_mmap_win32.c
@@ -1,3 +1,13 @@
+/**
+ * plat_mmap_win32.c
+ *
+ * Copyright (c) 2005, 2006
+ *      libchewing Core Team. See ChangeLog for details.
+ *
+ * See the file "COPYING" for information on usage and redistribution
+ * of this file.
+ */
+
 #ifdef HAVE_CONFIG_H
   #include <config.h>
 #endif
