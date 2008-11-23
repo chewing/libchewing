4a338ed96b9e2a2e6f4f376b30b7373374096c12
jserv <jserv@ac1be623-90ea-0310-9410-ba68b2efa777>
2008-11-23 18:02:27 +0000

    Syntax tweaks.
    git-svn-id: https://svn.csie.net/chewing/libchewing/trunk@989 ac1be623-90ea-0310-9410-ba68b2efa777

--- src/chewingutil.c
+++ src/chewingutil.c
@@ -597,27 +597,28 @@ static int ChewingIsBreakPoint( int cursor, ChewingData *pgdata )
 {
 	static char *break_word[] = {
 		"\xE6\x98\xAF", "\xE7\x9A\x84", "\xE4\xBA\x86", "\xE4\xB8\x8D",
-		/* 是的了不 */
+		/* 是              的              了              不 */
 		"\xE4\xB9\x9F", "\xE8\x80\x8C", "\xE4\xBD\xA0", "\xE6\x88\x91",
-		/* 也而你我 */
+		/* 也              而              你              我 */
 		"\xE4\xBB\x96", "\xE8\x88\x87", "\xE5\xAE\x83", "\xE5\xA5\xB9",
-		/* 他與它她 */
+		/* 他              與              它              她 */
 		"\xE5\x85\xB6", "\xE5\xB0\xB1", "\xE5\x92\x8C", "\xE6\x88\x96",
-		/* 其就和或 */
+		/* 其              就              和              或 */
 		"\xE5\x80\x91", "\xE6\x80\xA7", "\xE5\x93\xA1", "\xE5\xAD\x90",
-		/* 們性員子 */
+		/* 們              性              員              子 */
 		"\xE4\xB8\x8A", "\xE4\xB8\x8B", "\xE4\xB8\xAD", "\xE5\x85\xA7",
-		/* 上下中內 */
+		/* 上              下              中              內 */
 		"\xE5\xA4\x96", "\xE5\x8C\x96", "\xE8\x80\x85", "\xE5\xAE\xB6",
-		/* 外化者家 */
+		/* 外              化              者              家 */
 		"\xE5\x85\x92", "\xE5\xB9\xB4", "\xE6\x9C\x88", "\xE6\x97\xA5",
-		/* 兒年月日 */
+		/* 兒              年              月              日 */
 		"\xE6\x99\x82", "\xE5\x88\x86", "\xE7\xA7\x92", "\xE8\xA1\x97",
-		/* 時分秒街 */
+		/* 時              分              秒              街 */
 		"\xE8\xB7\xAF", "\xE6\x9D\x91",
-		/* 路村 */
-        "\xE5\x9C\xA8", };
-        /* 在 */
+		/* 路              村 */
+		"\xE5\x9C\xA8",
+		/* 在 */
+	};
 	char buf[ MAX_UTF8_SIZE + 1 ];
 	int i = 0, symbols = 0;
 	for ( i = 0; i < cursor; i++ )
@@ -628,9 +629,9 @@ static int ChewingIsBreakPoint( int cursor, ChewingData *pgdata )
 	else {
 		ueStrNCpy( buf,
 				ueStrSeek( (char *) &pgdata->phrOut.chiBuf, cursor ),
-				1, 1);
-		for ( i = 0; i < sizeof(break_word)/sizeof(break_word[0]); i++ ) {
-			if ( ! strcmp ( buf, break_word[i] ) )
+				1, 1 );
+		for ( i = 0; i < sizeof(break_word) / sizeof(break_word[0]); i++ ) {
+			if ( ! strcmp ( buf, break_word[ i ] ) )
 				return 1;
 		}
 	}
@@ -655,11 +656,11 @@ void AutoLearnPhrase( ChewingData *pgdata )
 					ueStrSeek( (char *) &pgdata->phrOut.chiBuf, from ),
 					len, 1);
 			prev_pos += len;
-            pending = 1;
+			pending = 1;
 		}
 		else {
 			if ( pending ) {
-                UserUpdatePhrase( bufPhoneSeq, bufWordSeq );
+				UserUpdatePhrase( bufPhoneSeq, bufWordSeq );
 				prev_pos = 0;
 				pending = 0;
 			}
@@ -671,11 +672,11 @@ void AutoLearnPhrase( ChewingData *pgdata )
 			UserUpdatePhrase( bufPhoneSeq, bufWordSeq );
 		}
 	}
-    if ( pending ) {
-        UserUpdatePhrase( bufPhoneSeq, bufWordSeq );
-        prev_pos = 0;
-        pending = 0;
-    }
+	if ( pending ) {
+		UserUpdatePhrase( bufPhoneSeq, bufWordSeq );
+		prev_pos = 0;
+		pending = 0;
+	}
 }
 
 int AddChi( uint16 phone, ChewingData *pgdata )
