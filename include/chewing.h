239c61ba0f64a102002d86d1ee345b2b9af9cf6e
jserv <jserv@ac1be623-90ea-0310-9410-ba68b2efa777>
2008-11-30 10:46:49 +0000

    API Documentation for high level descriptions.
    2008-11-30  Jim Huang  <jserv.tw@gmail.com>
    
            * Doxyfile.in: Update with doxygen 1.5.7.1 -u.  Switch lang to
            English.
    
            * include/chewing.h: Initially documented for high level descriptions.
    
    
    git-svn-id: https://svn.csie.net/chewing/libchewing/trunk@1012 ac1be623-90ea-0310-9410-ba68b2efa777

--- include/chewing.h
+++ include/chewing.h
@@ -1,7 +1,7 @@
 /*
  * chewing.h
  *
- * Copyright (c) 2004, 2005, 2006
+ * Copyright (c) 2004, 2005, 2006, 2008
  *	libchewing Core Team. See ChangeLog for details.
  *
  * See the file "COPYING" for information on usage and redistribution
@@ -18,14 +18,33 @@ extern "C" {
 /*! \file chewing.h
  *  \brief Chewing Headers
  *  \author libchewing Core Team
+ *
+ * <h1>Overview</h1>
+ * 
+ * As far as we expect, input method (in fact, the way for text input and
+ * output, especially in multi-lingual environments) implementations are
+ * becoming more and more complex in system integrations.  The emerging
+ * standard for practical and flexible development cycles is required, so that
+ * we are facing the impacts from various Chinese input method implementations
+ * and the integration into existing framework or system designs.  At the
+ * result, Chewing Input Method (abbreviated as "Chewing") attempts to be one
+ * of the approachesto solve and ease the problems with the inclusion of base
+ * classes, on the basis of cross-platform, and cross-operating-environment
+ * derivative classes, which are built as the abstract backbone of intelligent
+ * Chinese phonetic input method implementations.  From the perspectives,
+ * Chewing defines the abstract behavior how an intelligent phonetic IM should
+ * works via the common interface, and Chewing permits the extra parameters
+ * and properties for the input method implementations to extend their
+ * specific functionality.
  */
 
 #include "chewingio.h"
 #include "global.h"
 #include "mod_aux.h"
 
-/* new macro to indicate the internal encoding.
- * since libchewing 0.3.0 */
+/** @brief indicate the internal encoding.
+ *  @since 0.3.0
+ */
 #define LIBCHEWING_ENCODING "UTF-8"
 
 /* statistics symbol reported by chewing */
