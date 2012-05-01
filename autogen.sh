a528cc69ec1a82ea89cfb723257657e950dc0a5c
Kan-Ru Chen <kanru@kanru.info>
2012-05-01 16:40:19 +0800

    Use autoreconf.

--- autogen.sh
+++ autogen.sh
@@ -1,26 +1,8 @@
 #! /bin/sh
 
-set -x
-
-if [ "x${ACLOCAL_DIR}" != "x" ]; then
-	ACLOCAL_ARG="-I ${ACLOCAL_DIR}"
+AUTORECONF_ARGS=-i
+if [ -d m4 ]; then
+    AUTORECONF_ARGS="$AUTORECONF_ARGS -I m4"
 fi
 
-${ACLOCAL:-aclocal} ${ACLOCAL_ARG}
-
-# Darwin has different name for "libtoolize"
-UNAME=`uname`
-if [ "x${UNAME}" = "xDarwin" ]; then
-	${LIBTOOLIZE:-glibtoolize} -c --automake 
-else
-	${LIBTOOLIZE:-libtoolize} -c --automake 
-fi
-
-# intltoolize -c --automake
-${AUTOHEADER:-autoheader}
-${AUTOMAKE:-automake} --add-missing --copy --include-deps
-${AUTOCONF:-autoconf}
-
-if [ -d autom4te.cache ]; then
-	rm -rf autom4te.cache
-fi
+autoreconf $AUTORECONF_ARGS
