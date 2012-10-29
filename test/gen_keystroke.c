baa1220949321678bf80604a2b1f4d5818a8974d
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2012-10-29 10:43:14 +0800

    Use ax_with_curses.m4 from autoconf-archive

--- test/gen_keystroke.c
+++ test/gen_keystroke.c
@@ -20,10 +20,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifdef HAVE_NCURSESW_CURSES_H
-#include <ncursesw/curses.h>
+#if defined HAVE_NCURSESW_CURSES_H
+#  include <ncursesw/curses.h>
+#elif defined HAVE_NCURSESW_H
+#  include <ncursesw.h>
+#elif defined HAVE_NCURSES_CURSES_H
+#  include <ncurses/curses.h>
+#elif defined HAVE_NCURSES_H
+#  include <ncurses.h>
+#elif defined HAVE_CURSES_H
+#  include <curses.h>
 #else
-#error "There is no curses package found."
+#  error "SysV or X/Open-compatible Curses header file required"
 #endif
 #include <locale.h>
 
