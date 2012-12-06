50101fe213efaad10cb09beef9693e0ec63ee391
ChangZhuo Chen <czchen@gmail.com>
2012-12-06 23:50:11 +0800

    Fix 趕人 in tsi.src

---
+++ test/test-regression.c
@@ -0,0 +1,53 @@
+/**
+ * test-regression.c
+ *
+ * Copyright (c) 2012
+ *      libchewing Core Team. See ChangeLog for details.
+ *
+ * See the file "COPYING" for information on usage and redistribution
+ * of this file.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "chewing.h"
+#include "plat_types.h"
+#include "hash-private.h"
+#include "test.h"
+
+
+void test_libchewing_data_issue_1()
+{
+	const TestData DATA = {
+		.token = "e03y.3",
+		.expected = "趕走",
+	};
+	ChewingContext *ctx;
+
+	remove( TEST_HASH_DIR PLAT_SEPARATOR HASH_FILE );
+
+	chewing_Init( NULL, NULL );
+
+	ctx = chewing_new();
+	chewing_set_maxChiSymbolLen( ctx, 16 );
+	type_keystoke_by_string( ctx, DATA.token );
+	ok_preedit_buffer( ctx, DATA.expected );
+
+	chewing_delete( ctx );
+	chewing_Terminate();
+}
+
+int main()
+{
+	putenv( "CHEWING_PATH=" CHEWING_DATA_PREFIX );
+	putenv( "CHEWING_USER_PATH=" TEST_HASH_DIR );
+
+	test_libchewing_data_issue_1();
+
+	return exit_status();
+}
