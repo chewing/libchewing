ad9dd1163290b10d56316ddd78972cf575e36519
ChangZhuo Chen <czchen@gmail.com>
2012-12-15 21:31:17 +0800

    Add test case for libchewing issue 30

--- test/test-regression.c
+++ test/test-regression.c
@@ -20,6 +20,28 @@
 #include "hash-private.h"
 #include "test.h"
 
+void test_libchewing_issue_30()
+{
+	ChewingContext *ctx;
+	int cursor;
+
+	remove( TEST_HASH_DIR PLAT_SEPARATOR HASH_FILE );
+
+	chewing_Init( NULL, NULL );
+
+	ctx = chewing_new();
+	chewing_set_maxChiSymbolLen( ctx, 16 );
+	chewing_set_autoShiftCur( ctx, 1 );
+	chewing_set_spaceAsSelection( ctx, 1 );
+	chewing_set_phraseChoiceRearward( ctx, 1 );
+
+	type_keystoke_by_string( ctx, "hk4g4<H> 3 1" );
+	cursor = chewing_cursor_Current( ctx );
+	ok( cursor == 2, "cursor position `%d' shall be `2'", cursor);
+
+	chewing_delete( ctx );
+	chewing_Terminate();
+}
 
 void test_libchewing_data_issue_1()
 {
@@ -48,6 +70,7 @@ int main()
 	putenv( "CHEWING_USER_PATH=" TEST_HASH_DIR );
 
 	test_libchewing_data_issue_1();
+	test_libchewing_issue_30();
 
 	return exit_status();
 }
