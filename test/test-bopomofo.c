8ecf3a589dda800dad9751c408934082aad8953c
ChangZhuo Chen <czchen@gmail.com>
2013-02-12 13:44:42 +0800

    Add test cases for CtrlNum, PageUp, PageDown

--- test/test-bopomofo.c
+++ test/test-bopomofo.c
@@ -527,6 +527,11 @@ void test_Tab()
 	test_Tab_at_the_end();
 }
 
+void test_DblTab()
+{
+	/* FIXME: Implement this. */
+}
+
 void test_Capslock()
 {
 	ChewingContext *ctx;
@@ -587,6 +592,124 @@ void test_End()
 	chewing_Terminate();
 }
 
+void test_PageUp()
+{
+	ChewingContext *ctx;
+	int cursor;
+
+	chewing_Init( NULL, NULL );
+
+	ctx = chewing_new();
+	chewing_set_maxChiSymbolLen( ctx, 16 );
+
+	type_keystroke_by_string( ctx, "hk4g4<L><L>" );
+	cursor = chewing_cursor_Current( ctx );
+	ok( cursor == 0, "cursor `%d' shall be 0", cursor );
+
+	type_keystroke_by_string( ctx, "<PU>" );
+	cursor = chewing_cursor_Current( ctx );
+	ok( cursor == 2, "cursor `%d' shall be 2", cursor );
+
+	chewing_delete( ctx );
+	chewing_Terminate();
+}
+
+void test_PageDown()
+{
+	ChewingContext *ctx;
+	int cursor;
+
+	chewing_Init( NULL, NULL );
+
+	ctx = chewing_new();
+	chewing_set_maxChiSymbolLen( ctx, 16 );
+
+	type_keystroke_by_string( ctx, "hk4g4<L><L>" );
+	cursor = chewing_cursor_Current( ctx );
+	ok( cursor == 0, "cursor `%d' shall be 0", cursor );
+
+	type_keystroke_by_string( ctx, "<PD>" );
+	cursor = chewing_cursor_Current( ctx );
+	ok( cursor == 2, "cursor `%d' shall be 2", cursor );
+
+	chewing_delete( ctx );
+	chewing_Terminate();
+}
+
+void test_CtrlNum_add_phrase_right()
+{
+	static const char phrase[] = "\xE6\xB8\xAC\xE8\xA9\xA6" /* 測試 */;
+	static const char bopomofo[] = "\xE3\x84\x98\xE3\x84\x9C\xCB\x8B \xE3\x84\x95\xCB\x8B" /* ㄘㄜˋ ㄕˋ */;
+	int cursor;
+	ChewingContext *ctx;
+
+	remove( TEST_HASH_DIR PLAT_SEPARATOR HASH_FILE );
+
+	chewing_Init( NULL, NULL );
+
+	ctx = chewing_new();
+	chewing_set_maxChiSymbolLen( ctx, 16 );
+        chewing_set_addPhraseDirection( ctx, 0 );
+
+	ok( has_userphrase( ctx, bopomofo, phrase ) == 0,
+		"`%s' shall not be in userphrase", phrase );
+
+	type_keystroke_by_string( ctx, "hk4g4<H><C2>" );
+	ok_preedit_buffer( ctx, phrase );
+	cursor = chewing_cursor_Current( ctx );
+	ok( cursor == 0, "cursor position `%d' shall be 0", cursor );
+	ok( has_userphrase( ctx, bopomofo, phrase ) == 1,
+		"`%s' shall be in userphrase", phrase );
+
+	chewing_delete( ctx );
+	chewing_Terminate();
+}
+
+void test_CtrlNum_add_phrase_left()
+{
+	static const char phrase[] = "\xE6\xB8\xAC\xE8\xA9\xA6" /* 測試 */;
+	static const char bopomofo[] = "\xE3\x84\x98\xE3\x84\x9C\xCB\x8B \xE3\x84\x95\xCB\x8B" /* ㄘㄜˋ ㄕˋ */;
+	int cursor;
+	ChewingContext *ctx;
+
+	remove( TEST_HASH_DIR PLAT_SEPARATOR HASH_FILE );
+
+	chewing_Init( NULL, NULL );
+
+	ctx = chewing_new();
+	chewing_set_maxChiSymbolLen( ctx, 16 );
+        chewing_set_addPhraseDirection( ctx, 1 );
+
+	ok( has_userphrase( ctx, bopomofo, phrase ) == 0,
+		"`%s' shall not be in userphrase", phrase );
+
+	type_keystroke_by_string( ctx, "hk4g4<C2>" );
+	ok_preedit_buffer( ctx, phrase );
+	cursor = chewing_cursor_Current( ctx );
+	ok( cursor == 2, "cursor position `%d' shall be 2", cursor );
+	ok( has_userphrase( ctx, bopomofo, phrase ) == 1,
+		"`%s' shall be in userphrase", phrase );
+
+	chewing_delete( ctx );
+	chewing_Terminate();
+}
+
+void test_CtrlNum()
+{
+	test_CtrlNum_add_phrase_right();
+	test_CtrlNum_add_phrase_left();
+}
+
+void test_ShiftSpace()
+{
+	/* FIXME: Implement this. */
+}
+
+void test_Numlock()
+{
+	/* FIXME: Implement this. */
+}
+
 void test_get_phoneSeq()
 {
 	static const unsigned short PHONE[] = { 10268, 8708 };
@@ -629,9 +752,15 @@ int main()
 	test_ShiftLeft();
 	test_ShiftRight();
 	test_Tab();
+	test_DblTab();
 	test_Capslock();
 	test_Home();
 	test_End();
+	test_PageUp();
+	test_PageDown();
+	test_CtrlNum();
+	test_ShiftSpace();
+	test_Numlock();
 
 	test_get_phoneSeq();
 
