1da1238c820ff4d0b69f8916a7bb8d0329511bbd
ChangZhuo Chen <czchen@gmail.com>
2013-06-24 18:35:32 +0800

    Inline memory access function

--- include/internal/memory-private.h
+++ include/internal/memory-private.h
@@ -18,8 +18,62 @@
 #  include <stdint.h>
 #endif
 
-uint16_t GetUint16( const char *ptr );
-void PutUint16( uint16_t val, char *ptr );
+static inline uint16_t GetUint16( const char *ptr )
+{
+	uint16_t val;
+#if WORDS_BIGENDIAN
+	val =
+		( ptr[0] << 8 ) |
+		( ptr[1] << 0 );
+#else
+	val =
+		( ptr[0] << 0 ) |
+		( ptr[1] << 8 );
+#endif
+	return val;
+}
 
-int GetInt32( const char *ptr );
-void PutInt32( int val, char *ptr );
+static inline void PutUint16( uint16_t val, char *ptr )
+{
+#if WORDS_BIGENDIAN
+	ptr[0] = ( val >> 8 ) & 0xff;
+	ptr[1] = ( val >> 0 ) & 0xff;
+#else
+	ptr[0] = ( val >> 0 ) & 0xff;
+	ptr[1] = ( val >> 8 ) & 0xff;
+#endif
+}
+
+static inline int GetInt32( const char *ptr )
+{
+	int val;
+#if WORDS_BIGENDIAN
+	val =
+		( ptr[0] << 24 ) |
+		( ptr[1] << 16 ) |
+		( ptr[2] <<  8 ) |
+		( ptr[3] <<  0 );
+#else
+	val =
+		( ptr[0] <<  0 ) |
+		( ptr[1] <<  8 ) |
+		( ptr[2] << 16 ) |
+		( ptr[3] << 24 );
+#endif
+	return val;
+}
+
+static inline void PutInt32( int val, char *ptr )
+{
+#if WORDS_BIGENDIAN
+	ptr[0] = ( val >> 24 ) & 0xff;
+	ptr[1] = ( val >> 16 ) & 0xff;
+	ptr[2] = ( val >>  8 ) & 0xff;
+	ptr[3] = ( val >>  0 ) & 0xff;
+#else
+	ptr[0] = ( val >>  0 ) & 0xff;
+	ptr[1] = ( val >>  8 ) & 0xff;
+	ptr[2] = ( val >> 16 ) & 0xff;
+	ptr[3] = ( val >> 24 ) & 0xff;
+#endif
+}
