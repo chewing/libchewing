84fe4ded25f4092b0eecd60615dec0c0b130605b
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-07-05 11:14:45 +0800

    Fix wrd_ptr->word to wrd_ptr->word[0] for Str2Word(), considering type consistency.

--- src/char.c
+++ src/char.c
@@ -184,7 +184,7 @@ static void Str2Word( ChewingData *pgdata, Word *wrd_ptr )
 	fgettab( buf, 1000, pgdata->static_data.charfile );
 	/* only read 6 bytes to wrd_ptr->word avoid buffer overflow */
 	sscanf( buf, "%hu %6[^ ]", &sh, wrd_ptr->word );
-	assert( wrd_ptr->word != '\0' );
+	assert( wrd_ptr->word[0] != '\0' );
 #else
 	unsigned char size;
 	size = *(unsigned char *) pgdata->static_data.char_cur_pos;
