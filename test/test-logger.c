9e557b0d05655ba05e919663b580a0d982b0fdca
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2013-10-27 05:47:45 +0800

    ADd test-logger.c back for NULL logger test

---
+++ test/test-logger.c
@@ -0,0 +1,55 @@
+/**
+ * test-logger.c
+ *
+ * Copyright (c) 2013
+ *      libchewing Core Team. See ChangeLog for details.
+ *
+ * See the file "COPYING" for information on usage and redistribution
+ * of this file.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "chewing.h"
+#include "testhelper.h"
+
+FILE *fd;
+
+void test_set_null_logger()
+{
+	ChewingContext *ctx;
+
+	ctx = chewing_new();
+	start_testcase( ctx, fd );
+
+	chewing_set_logger( ctx, NULL, 0 );
+	type_keystroke_by_string( ctx, "hk4g4" );
+
+	chewing_delete( ctx );
+}
+
+int main( int argc, char *argv[] )
+{
+	char *logname;
+
+	putenv( "CHEWING_PATH=" CHEWING_DATA_PREFIX );
+	putenv( "CHEWING_USER_PATH=" TEST_HASH_DIR );
+
+	asprintf( &logname, "%s.log", argv[0] );
+	fd = fopen( logname, "w" );
+	assert( fd );
+	free( logname );
+
+	test_set_null_logger();
+
+	fclose( fd );
+
+	return exit_status();
+}
