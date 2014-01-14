6df140312ae3a5fb073e335f091dab9dd6b4cb2e
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-01-14 20:18:53 +0800

    Fix warning

--- src/userphrase-hash.c
+++ src/userphrase-hash.c
@@ -212,7 +212,7 @@ int UserRemovePhrase( ChewingData *pgdata, const uint16_t phoneSeq[], const char
 			*prev = item->next;
 			FreeHashItem( item );
 
-			return;
+			return 0;
 		}
 		prev = &item->next;
 		item = item->next;
