e05c670fc7a398dfd8b3848a914a4b09f5cc03e2
Jim Huang <jserv@0xlab.org>
2014-04-02 21:09:25 +0800

    chewing-compat: trivial syntax tweak

--- include/chewing-compat.h
+++ include/chewing-compat.h
@@ -19,15 +19,20 @@
 #define LIBCHEWING_ENCODING "UTF-8"
 
 /* deprecated function. for API compatibility */
-CHEWING_API int chewing_zuin_Check(ChewingContext *ctx) DEPRECATED_FOR(chewing_bopomofo_Check);
-CHEWING_API char *chewing_zuin_String(ChewingContext *,
-                                      int *zuin_count) DEPRECATED_FOR(chewing_bopomofo_String_static);
+CHEWING_API int chewing_zuin_Check(ChewingContext *ctx)
+    DEPRECATED_FOR(chewing_bopomofo_Check);
+CHEWING_API char *chewing_zuin_String(ChewingContext *, int *zuin_count)
+    DEPRECATED_FOR(chewing_bopomofo_String_static);
 
-CHEWING_API int chewing_Init(const char *dataPath, const char *hashPath) DEPRECATED;
+CHEWING_API int chewing_Init(const char *dataPath, const char *hashPath)
+    DEPRECATED;
 CHEWING_API void chewing_Terminate() DEPRECATED;
-CHEWING_API int chewing_Configure(ChewingContext *ctx, ChewingConfigData * pcd) DEPRECATED_FOR(chewing_set_*);
-CHEWING_API void chewing_set_hsuSelKeyType(ChewingContext *ctx, int mode) DEPRECATED_FOR(chewing_set_selKey);
-CHEWING_API int chewing_get_hsuSelKeyType(ChewingContext *ctx) DEPRECATED_FOR(chewing_get_selKey);
+CHEWING_API int chewing_Configure(ChewingContext *ctx, ChewingConfigData * pcd)
+    DEPRECATED_FOR(chewing_set_*);
+CHEWING_API void chewing_set_hsuSelKeyType(ChewingContext *ctx, int mode)
+    DEPRECATED_FOR(chewing_set_selKey);
+CHEWING_API int chewing_get_hsuSelKeyType(ChewingContext *ctx)
+    DEPRECATED_FOR(chewing_get_selKey);
 
 /* *INDENT-OFF* */
 #endif
