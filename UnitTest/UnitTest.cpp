607e3e438c106bbb7704fb3a45781ea4f2767288
mnpan <sa69mo@gmail.com>
2017-04-12 00:26:38 +0800

    c3: add call /libchewing/test/testhelper.h

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,17 @@
+#include <chewing.h>
+#include <testhelper.h>
+#include <iostream>
+using namespace std;
+
+int main( int argc, char** argv){
+	testing::InitGoogleTest( &argc, argv);
+	return RUN_ALL_TESTS();
+	ChewingContext* ctx = chewing_new();
+	type_keystroke_by_string( ctx, "su3cl3");
+  
+	if( chewing_commit_Check(ctx)){
+		cout << chewing_commit_String(ctx) << endl;
+	}
+	cout << "finish" << endl;
+	return 0;
+}
