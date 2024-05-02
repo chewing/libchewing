c09a370c0b16b198a7e014c739ac31469eab7606
Kan-Ru Chen <kanru@kanru.info>
2024-05-02 15:40:28 +0900

    docs(cli): Use clap_mangen to generate manpages

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -7,6 +7,17 @@ mod info;
 mod init_database;
 
 fn main() -> Result<()> {
+    #[cfg(feature = "mangen")]
+    {
+        use clap::CommandFactory;
+        if let Ok(_) = std::env::var("UPDATE_MANPAGE") {
+            clap_mangen::generate_to(
+                flags::ChewingCli::command(),
+                std::env::args().nth(1).unwrap(),
+            )?;
+            return Ok(());
+        }
+    }
     let cli = flags::ChewingCli::parse();
     match cli.subcommand {
         flags::ChewingCliCmd::InitDatabase(args) => init_database::run(args)?,
