9c168a85cc548350c86d5deb3cf2e1068624d09f
Kan-Ru Chen <kanru@kanru.info>
2024-05-31 08:16:51 +0900

    refactor(editor): Simplify key_seq handling

--- src/editor/zhuyin_layout/pinyin.rs
+++ src/editor/zhuyin_layout/pinyin.rs
@@ -88,36 +88,10 @@ impl SyllableEditor for Pinyin {
                 // buffer is full, ignore this keystroke
                 return KeyBehavior::NoWord;
             }
-            let ch = match key.code {
-                KeyCode::A => 'a',
-                KeyCode::B => 'b',
-                KeyCode::C => 'c',
-                KeyCode::D => 'd',
-                KeyCode::E => 'e',
-                KeyCode::F => 'f',
-                KeyCode::G => 'g',
-                KeyCode::H => 'h',
-                KeyCode::I => 'i',
-                KeyCode::J => 'j',
-                KeyCode::K => 'k',
-                KeyCode::L => 'l',
-                KeyCode::M => 'm',
-                KeyCode::N => 'n',
-                KeyCode::O => 'o',
-                KeyCode::P => 'p',
-                KeyCode::Q => 'q',
-                KeyCode::R => 'r',
-                KeyCode::S => 's',
-                KeyCode::T => 't',
-                KeyCode::U => 'u',
-                KeyCode::V => 'v',
-                KeyCode::W => 'w',
-                KeyCode::X => 'x',
-                KeyCode::Y => 'y',
-                KeyCode::Z => 'z',
-                _ => return KeyBehavior::KeyError,
-            };
-            self.key_seq.push(ch);
+            if !key.unicode.is_ascii_alphabetic() {
+                return KeyBehavior::KeyError;
+            }
+            self.key_seq.push(key.unicode);
             return KeyBehavior::Absorb;
         }
 
