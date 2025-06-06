e70507c00518e89cb5820de97f304ff0f4e39b7a
Rumen Cholakov <rcholakov@abaltatech.com>
2025-06-06 11:05:45 +0300

    Add sample app

---
+++ swift/Examples/ChewingSampleApp/chewing/chewingApp.swift
@@ -0,0 +1,17 @@
+//
+//  chewingApp.swift
+//  chewing
+//
+//  Created by Rumen Cholakov on 2.06.25.
+//
+
+import SwiftUI
+
+@main
+struct chewingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift/Examples/ChewingSampleApp/chewingTests/chewingTests.swift
@@ -0,0 +1,17 @@
+//
+//  chewingTests.swift
+//  chewingTests
+//
+//  Created by Rumen Cholakov on 2.06.25.
+//
+
+import Testing
+@testable import chewing
+
+struct chewingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
