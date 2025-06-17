0ee4d930d84e377f5ba27212c85bedd97d207731
Ivan Marinov <imarinov@abaltatech.com>
2025-06-17 10:31:05 +0300

    Compile and package the library for use in an iOS application (#6)

--- swift/chewing-simplified/Package.swift
+++ swift/chewing-simplified/Package.swift
@@ -1,6 +1,9 @@
 // swift-tools-version:5.9
 import PackageDescription
 
+let version = "1.0.1"
+let checksum = "6bb037a2e242e87397be298f372d20d61f4a6ee109e2e68a0d7c19fd9af0905f"
+
 let package = Package(
     name: "Chewing",
     platforms: [
@@ -19,7 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CLibChewing",
-            path: "libchewing.xcframework"
+            url: "https://github.com/abaltatech/libchewing/releases/download/\(version)/libchewing.xcframework.zip",
+            checksum: checksum
         ),
         .target(
             name: "Chewing",
