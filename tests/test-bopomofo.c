d75f88dce4269803af224dc45e395805d4d162dd
Kan-Ru Chen <kanru@kanru.info>
2025-07-03 20:24:02 +0900

    feat: always auto-commit in simple mode

--- tests/test-bopomofo.c
+++ tests/test-bopomofo.c
@@ -1396,10 +1396,14 @@ void test_FuzzySearchMode_Hanyu()
 void test_SimpleEngine()
 {
     const TestData SIMPLE_INPUT[] = {
-        {"ru03120 15j41up 1ai61g41!<E>", "簡單住因模市！" },
-        {"ru03<EE>20 <EE>5j4<EE>up <EE>ai6<EE>g4<EE>!<E>", "簡單住因模市！" },
-        {"ru03120 15j44up 2ai61g4<D>2!<E>", "簡單注音模式！" },
-        {"ru03120 15j44up 2ai61g4<D>2!<H>20 1tjp61<E>", "單純簡單注音模式！" },
+        {"ru031", "簡" },
+        {"20 1", "單" },
+        {"5j41", "住" },
+        {"up 1", "因" },
+        {"ai61", "模" },
+        {"g41", "市" },
+        {"!", "！" },
+        {"ru03<EE>", "簡" },
     };
     size_t i;
     ChewingContext *ctx;
