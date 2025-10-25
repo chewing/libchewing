c0710ec08f338b045aacf3a9891335d9aa4c9f64
Kan-Ru Chen <kanru@kanru.info>
2025-10-25 20:28:24 +0900

    chore(release): bump version to 0.11.0-alpha.1

--- capi/src/version.rs
+++ capi/src/version.rs
@@ -1,12 +1,12 @@
 use std::ffi::{c_char, c_int};
 
 pub const CHEWING_VERSION_MAJOR: c_int = 0;
-pub const CHEWING_VERSION_MINOR: c_int = 10;
-pub const CHEWING_VERSION_PATCH: c_int = 3;
+pub const CHEWING_VERSION_MINOR: c_int = 11;
+pub const CHEWING_VERSION_PATCH: c_int = 0;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn chewing_version() -> *const c_char {
-    c"0.10.3".as_ptr()
+    c"0.11.0-alpha.1".as_ptr()
 }
 
 #[unsafe(no_mangle)]
@@ -26,5 +26,5 @@ pub extern "C" fn chewing_version_patch() -> c_int {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn chewing_version_extra() -> *const c_char {
-    c"".as_ptr()
+    c"alpha.1".as_ptr()
 }
